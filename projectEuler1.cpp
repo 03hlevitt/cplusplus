7f34873bb45a70152d03251a9fe17f17bd6204cc
03hlevitt <03hlevitt@gmail.com>
2022-10-11 15:28:07 +0100

    project euler problem one

--- projectEuler1.cpp
+++ projectEuler1.cpp
@@ -4,14 +4,28 @@ using std::cout;
 using std::endl;
 #include <string>
 using std::string;
+
+int calculation(int x, int y);
+
 int main()
 {
-    cout << "This program is designed to tackle project Euler's first problem. \nPlease enter a number below a thousand:" << endl;
-    double x;
-    double y;
+    cout << "This program is designed to tackle project Euler's first problem. \nPlease enter an integer below a thousand:" << endl;
+    int x;
+    int y;
     cin >> x;
-    cout << "Please enter a second number:" << endl;
+    cout << "Please enter a second integer:" << endl;
     cin >> y;
-    cout << "Now we will calculate the sum of the multiples of " << x << " and " << y << " below a 1000!" << endl;
+    cout << "The sum of the multiples of " << x << " and " << y << " is: " << calculation(x,y) << endl;
     return 0;
+}
+
+int calculation(int x, int y)
+{
+    int z = 0;
+    for (int i = 1; i <1000; ++i) {
+        if (i % x == 0 or i % y == 0) {
+            z += i;
+        }
+    }
+    return z;
 }
